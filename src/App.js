478de332fd4f4f57b623fa528533f1c1592e2b98
Your Name <you@example.com>
2021-04-09 14:55:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import image from './img/pika.png';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+        <h2>Author: Detective Pikachu</h2>
+        <h2>Year: 2021</h2>
+        <h2>Weather: mist</h2>
+      <img src={image} alt="profile-image" />
+    </div>
+  );
+}
+
+export default App;

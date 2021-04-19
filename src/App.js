f6073e9ed69b191d60bcb089e810bd185a266fab
Your Name <you@example.com>
2021-04-19 15:30:06 +0300

    Changed weather to hailstorm

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>My React App</h1>
         <h2>Author: Detective Pikachu</h2>
         <h2>Year: 2021</h2>
-        <h2>Weather: sunny</h2>
+        <h2>Weather: hailstorm</h2>
       <img src={image} alt="profile-image" />
     </div>
   );
